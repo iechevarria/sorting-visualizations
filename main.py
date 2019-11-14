f2908e28d71fce4bf109b8267755150680873275
Ivan Echevarria <ivan@echevarria.io>
2019-11-13 22:22:51 -0800

    working static visualization!!!

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-list_to_sort = [1, 3, 4, 5, 1, 3, 2, 5]
+from random import shuffle
+
+from matplotlib import cm
+
+viridis = cm.get_cmap('winter')
+
+
+list_to_sort = list(range(30))
+shuffle(list_to_sort)
 
 
 # data
@@ -7,6 +15,7 @@ class knowledge_arr():
         self.arr = arr
         self.looking_idxs = set()
         self.length = len(arr)
+        self.arr_history = [arr.copy()]
 
 
     def get(self, idx):
@@ -28,7 +37,7 @@ class knowledge_arr():
         tmp = self.arr[idx1]
         self.arr[idx1] = self.arr[idx2]
         self.arr[idx2] = tmp
-
+        self.arr_history.append(self.arr.copy())
 
 
 # display stuff
@@ -48,6 +57,63 @@ def print_annoted_arr(k_arr):
         ])
     )
 
+
+def print_svg(k_arr):
+    num_vals = len(k_arr.arr)
+    colors = {
+        val: tuple(int(v * 255) for v in viridis(idx / (num_vals - 1)))[:3]
+        for idx, val in enumerate(k_arr.arr)
+    }
+
+    # pad history at the beginning and end to make my life easier
+    padded_history = k_arr.arr_history.copy()
+    padded_history.insert(0, padded_history[0])
+    padded_history.append(padded_history[-1])
+
+    spacing = 2
+    line_width = 10
+    line_height = 40
+    total_width = (num_vals + 1) * spacing + num_vals * line_width
+    total_height = (len(padded_history) - 1) * line_height
+
+    svg = f"""
+        <!DOCTYPE html>
+        <html>
+        <body>
+        <svg width="{total_width}" height="{total_height}">
+    """
+
+    for idx, tup in enumerate(zip(padded_history, padded_history[1:])):
+        pre, post = tup
+
+        paths = ''
+
+        for preidx, val in enumerate(pre):
+            r, g, b = colors[val]
+            postidx = post.index(val)
+            x1 = preidx * spacing + (preidx) * line_width 
+            y1 = idx * line_height
+            x2 = preidx * spacing + (preidx + 1) * line_width
+            y2 = idx * line_height
+            x3 = postidx * spacing + (postidx + 1) * line_width
+            y3 = (idx + 1) * line_height
+            x4 = postidx * spacing + (postidx) * line_width
+            y4 = (idx + 1) * line_height
+
+            paths = (
+                f'<path d="M {x1},{y1} L {x2},{y2} L {x3},{y3} L {x4},{y4}' 
+                f'L {x1},{y1} z" fill="rgba({r},{g},{b},1.0)"/>\n'
+            ) + paths
+
+        svg += paths
+
+    svg += """
+        </svg>
+        </body>
+        </html>
+    """
+    with open("output.html", "w+") as text_file:
+        text_file.write(svg)
 
 
 # sorting
@@ -69,6 +135,9 @@ def sort_thing(thing):
 
         i += 1    
         print_annoted_arr(thing)
+
+
+    print_svg(thing)
 
 
 
