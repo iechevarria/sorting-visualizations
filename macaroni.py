fe37a6642a76161759fb5c319c5c4772868bc00a
Ivan Echevarria <ivan@echevarria.io>
2019-12-11 00:18:14 -0800

    [#3] clean up code a bit

--- macaroni.py
+++ macaroni.py
@@ -8,9 +8,11 @@ class Circle:
         self.radius = radius
 
 
-def get_inner_tangent_points(xp, yp, x, y, r):
-    # equations from http://www.ambrsoft.com/TrigoCalc/Circles2/Circles2Tangent_.htm
-
+def get_inner_tangents(xp, yp, x, y, r):
+    """Gets both inner tangent points for one circle
+    
+    equations from http://www.ambrsoft.com/TrigoCalc/Circles2/Circles2Tangent_.htm
+    """
     xp_a = xp - x
     yp_b = yp - y
     xp_a_2 = xp_a ** 2
@@ -34,7 +36,8 @@ def get_inner_tangent_points(xp, yp, x, y, r):
     return (xt1, yt1), (xt2, yt2)
 
 
-def get_inner_tangents(c1, c2):
+def get_tangent_lines(c1, c2):
+    """Gets both inner tangent points for each circle"""
     # equations from http://www.ambrsoft.com/TrigoCalc/Circles2/Circles2Tangent_.htm
 
     a = c1.x
@@ -47,8 +50,8 @@ def get_inner_tangents(c1, c2):
     xp = (a * r0 + c * r1) / (r0 + r1)
     yp = (b * r0 + d * r1) / (r0 + r1)
 
-    c1t1, c1t2 = get_inner_tangent_points(xp, yp, a, b, r0)
-    c2t1, c2t2 = get_inner_tangent_points(xp, yp, c, d, r1)
+    c1t1, c1t2 = get_inner_tangents(xp, yp, a, b, r0)
+    c2t1, c2t2 = get_inner_tangents(xp, yp, c, d, r1)
 
     return (c1t1, c2t1), (c1t2, c2t2)
 
@@ -65,10 +68,9 @@ def circle_to_svg(c):
     """
 
 
-def line_to_svg(pt1, pt2, stroke_width=10, stroke_color="red", stroke_linecap="round"):
-    x1, y1 = pt1
-    x2, y2 = pt2
-
+def make_line_svg(
+    x1, y1, x2, y2, stroke_width=10, stroke_color="red", stroke_linecap="round"
+):
     return (
         f'<line x1="{round(x1, 4)}" y1="{round(y1, 4)}"'
         f' x2="{round(x2, 4)}" y2="{round(y2, 4)}"'
@@ -78,15 +80,21 @@ def line_to_svg(pt1, pt2, stroke_width=10, stroke_color="red", stroke_linecap="r
     )
 
 
-def arc_to_svg(
-    pt1, pt2, r, arc_flag, stroke_width=10, stroke_color="red", stroke_linecap="round"
+def make_arc_svg(
+    pt1,
+    pt2,
+    radius,
+    arc_flag,
+    stroke_width=10,
+    stroke_color="red",
+    stroke_linecap="round",
 ):
     x1, y1 = pt1
     x2, y2 = pt2
 
     return (
-        f'<path d="M {round(x1, 4)} {round(y1, 4)} A {round(r, 4)}'
-        f' {round(r, 4)} 0 0 {arc_flag} {round(x2, 4)} {round(y2, 4)}"'
+        f'<path d="M {round(x1, 4)} {round(y1, 4)} A {round(radius, 4)}'
+        f' {round(radius, 4)} 0 0 {arc_flag} {round(x2, 4)} {round(y2, 4)}"'
         f' fill="transparent"'
         f' stroke="{stroke_color}"'
         f' stroke-linecap="{stroke_linecap}"'
@@ -95,13 +103,11 @@ def arc_to_svg(
 
 
 def choose_tangent_line(tl1, tl2):
-    # this is pretty hacky, it should really be done relative to line that
-    # passes through the center of both circles. I just have the luxury of
-    # never running into edge cases.
+    # this is pretty hacky but I have luxury of never running into edge cases
     return tl1 if tl1[0] < tl2[0] else tl2
 
 
-def get_double_macaroni_connection_svg(
+def make_double_macaroni_connection_svg(
     x1,
     y1,
     x2,
@@ -119,44 +125,50 @@ def get_double_macaroni_connection_svg(
     else:
         ax, ay, bx, by = x2, y2, x1, y1
 
+    # set up circles for inner tangent calculations
     radius_midpoint = radius + (stroke_width / 2)
     a = Circle(ax + radius_midpoint, ay, radius_midpoint)
     b = Circle(bx - radius_midpoint, by, radius_midpoint)
 
+    # get the points that define the tangent line we care about
+    p1, p2 = choose_tangent_line(*get_tangent_lines(a, b))
+
+    # set up kwargs for svg drawing
+    arc1_kwargs = {
+        "pt1": (ax, ay),
+        "pt2": p1,
+        "radius": a.radius,
+    }
+    arc2_kwargs = {
+        "pt1": (bx, by),
+        "pt2": p2,
+        "radius": b.radius,
+    }
+    bg_kwargs = {
+        "stroke_width": stroke_width + outline_width,
+        "stroke_color": "white",
+        "stroke_linecap": "flat",
+    }
+    fg_kwargs = {
+        "stroke_width": stroke_width,
+        "stroke_color": stroke_color,
+        "stroke_linecap": stroke_linecap,
+    }
     arc_flag = int(a.y > b.y)
 
-    tangent_lines = get_inner_tangents(a, b)
-    p1, p2 = choose_tangent_line(tangent_lines[0], tangent_lines[1])
-
-    svg = ""
-
-    if display_circles:
-        svg += circle_to_svg(a) + circle_to_svg(b)
-
-    # background
-    svg += line_to_svg(p1, p2, stroke_width + outline_width, "white", "round")
-    svg += arc_to_svg(
-        (ax, ay), p1, a.radius, arc_flag, stroke_width + outline_width, "white", "flat"
+    return (
+        make_line_svg(*p1, *p2, **{**bg_kwargs, "stroke_linecap": "round"})
+        + make_arc_svg(**arc1_kwargs, **bg_kwargs, arc_flag=arc_flag)
+        + make_arc_svg(**arc2_kwargs, **bg_kwargs, arc_flag=arc_flag)
+        + make_line_svg(*p1, *p2, **fg_kwargs)
+        + make_arc_svg(**arc1_kwargs, **fg_kwargs, arc_flag=arc_flag)
+        + make_arc_svg(**arc2_kwargs, **fg_kwargs, arc_flag=arc_flag)
     )
-    svg += arc_to_svg(
-        (bx, by), p2, b.radius, arc_flag, stroke_width + outline_width, "white", "flat"
-    )
-
-    # foreground
-    svg += line_to_svg(p1, p2, stroke_width, stroke_color, stroke_linecap)
-    svg += arc_to_svg(
-        (ax, ay), p1, a.radius, arc_flag, stroke_width, stroke_color, stroke_linecap
-    )
-    svg += arc_to_svg(
-        (bx, by), p2, b.radius, arc_flag, stroke_width, stroke_color, stroke_linecap
-    )
-
-    return svg
 
 
 if __name__ == "__main__":
     inner_svg = ""
-    inner_svg += get_double_macaroni_connection_svg(
+    inner_svg += make_double_macaroni_connection_svg(
         x1=60,
         y1=60,
         x2=200,
@@ -166,6 +178,11 @@ if __name__ == "__main__":
         stroke_color="red",
         stroke_linecap="flat",
     )
+
+    svg = ""
+    if display_circles:
+        svg = circle_to_svg(a) + circle_to_svg(b)
+
 
     with open("tangent_test.html", "w+") as text_file:
         text_file.write(
--- svg.py
+++ svg.py
@@ -2,25 +2,25 @@ from itertools import chain
 
 from matplotlib import cm
 
-from macaroni import get_double_macaroni_connection_svg
+from macaroni import make_line_svg, make_double_macaroni_connection_svg
 
 
 def unnest_svg_list(svg_list):
     return "\n".join(list(chain(*svg_list)))
 
 
-def get_color_dict(end_state):
-    num_vals = len(end_state)
+def get_color_dict(sorted_list, cmap="magma_r"):
+    num_vals = len(sorted_list)
 
     cmap = cm.get_cmap("magma_r")
     return {
         val: tuple(int(v * 255) for v in cmap(0.075 + 0.8 * idx / (num_vals - 1)))[:3]
-        for idx, val in enumerate(end_state)
+        for idx, val in enumerate(sorted_list)
     }
 
 
-def get_color(colors, val):
-    r, g, b = colors[val]
+def get_color(color_dict, val):
+    r, g, b = color_dict[val]
     return f"rgba({r},{g},{b},1.0)"
 
 
@@ -51,8 +51,10 @@ def get_swaps_and_straights(history):
     return swaps, straights
 
 
-def transform_coords(coords, spacing, line_width, line_height, y_offset):
-    p1, p2 = coords
+def indices_to_coords(indices, spacing, line_width, line_height, y_offset):
+    """Transforms list indices into svg coordinates"""
+
+    p1, p2 = indices
     x1, y1 = p1
     x2, y2 = p2
 
@@ -64,29 +66,30 @@ def transform_coords(coords, spacing, line_width, line_height, y_offset):
     return xt1, yt1, xt2, yt2
 
 
-def make_straight_path(val, coords, color_dict, transform_kwargs):
-    x1, y1, x2, y2 = transform_coords(coords, **transform_kwargs)
+def make_straight_path(val, indices, color_dict, transform_kwargs):
+    """Creates an svg of a straight path"""
+
+    coords = indices_to_coords(indices, **transform_kwargs)
     stroke_color = get_color(color_dict, val)
     stroke_width = transform_kwargs["line_width"]
+    stroke_linecap = "round"
 
-    return (
-        f'<line x1="{x1}" y1="{y1}" x2="{x2}" y2="{y2}"'
-        f' stroke="{stroke_color}" stroke-linecap="round"'
-        f' stroke-width="{stroke_width}"/>'
-    )
+    return make_line_svg(*coords, stroke_width, stroke_color, stroke_linecap)
 
 
 def make_swap_path(
-    val, coords, color_dict, transform_kwargs, min_curve_radius, curve_radius_delta
+    val, indices, color_dict, transform_kwargs, min_curve_radius, curve_radius_delta
 ):
-    x1, y1, x2, y2 = transform_coords(coords, **transform_kwargs)
+    """Creates an svg of a swap path"""
+
+    x1, y1, x2, y2 = indices_to_coords(indices, **transform_kwargs)
     stroke_color = get_color(color_dict, val)
 
-    distance = abs(coords[0][0] - coords[1][0])
+    distance = abs(indices[0][0] - indices[1][0])
     curve_radius = min_curve_radius + curve_radius_delta / distance
     stroke_width = transform_kwargs["line_width"]
 
-    return get_double_macaroni_connection_svg(
+    return make_double_macaroni_connection_svg(
         x1,
         y1,
         x2,
@@ -97,16 +100,30 @@ def make_swap_path(
     )
 
 
+def make_straight_paths(straights, color_dict, transform_kwargs):
+    return unnest_svg_list(
+        [
+            [
+                make_straight_path(val, indices, color_dict, transform_kwargs)
+                for indices in list_indices
+            ]
+            for val, list_indices in straights.items()
+        ]
+    )
+
+
 def make_swap_paths(swaps, mode, color_dict, transform_kwargs, curve_kwargs):
     a, b = (0, 1) if mode == "over" else (1, 0)
     return unnest_svg_list(
         [
             [
-                make_swap_path(val, coord, color_dict, transform_kwargs, **curve_kwargs)
-                for coord in coords[1:-1]
-                if coord[a][0] < coord[b][0]
+                make_swap_path(
+                    val, indices, color_dict, transform_kwargs, **curve_kwargs
+                )
+                for indices in list_indices[1:-1]
+                if indices[a][0] < indices[b][0]
             ]
-            for val, coords in swaps.items()
+            for val, list_indices in swaps.items()
         ]
     )
 
@@ -136,27 +153,22 @@ def generate(
         "curve_radius_delta": line_height - line_width - spacing - min_curve_radius,
     }
 
-    colors = get_color_dict(history[-1])
+    # set up colors
+    color_dict = get_color_dict(history[-1])
+
+    # get path histories
     swaps, straights = get_swaps_and_straights(history)
 
     # actually make the svgs
-    straight_paths = unnest_svg_list(
-        [
-            [
-                make_straight_path(val, coord, colors, transform_kwargs)
-                for coord in coords
-            ]
-            for val, coords in straights.items()
-        ]
-    )
     swap_kwargs = {
         "swaps": swaps,
-        "color_dict": colors,
+        "color_dict": color_dict,
         "transform_kwargs": transform_kwargs,
         "curve_kwargs": curve_kwargs,
     }
     under_swap_paths = make_swap_paths(mode="under", **swap_kwargs)
     over_swap_paths = make_swap_paths(mode="over", **swap_kwargs)
+    straight_paths = make_straight_paths(straights, color_dict, transform_kwargs)
 
     # compute svg dimensions
     num_vals = len(history[0])
